c7802f8c6af9b49bb3a996ccb71b9a6aab4ce61d
fedec1 <federico.colantuono@edu.itspiemonte.it>
2024-06-12 17:51:52 +0200

    aggiornamento classe Nazione

--- src/main/java/com/fedec/entities/Nazione.java
+++ src/main/java/com/fedec/entities/Nazione.java
@@ -1,13 +1,83 @@
 package com.fedec.entities;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "countriesv2")
+@Table(name = "nazioni")
 public class Nazione {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	@Column(name = "name", nullable = false, length = 89)
+	private String name;
+	
+	@Column(name = "capital", nullable = false, length = 89)
+    private String capital;
+	
+	@Column(name = "flag", length = 255)
+    private String bandiera;
+	
+	@Column(name = "borders")
+    private String borders;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCapital() {
+		return capital;
+	}
+
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+
+	public String getBandiera() {
+		return bandiera;
+	}
+
+	public void setBandiera(String bandiera) {
+		this.bandiera = bandiera;
+	}
+
+	public String getBorders() {
+		return borders;
+	}
+
+	
+	public List<String> getBordersAsList() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(this.borders, List.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+	
+	
 }
