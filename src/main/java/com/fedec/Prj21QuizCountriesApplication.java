9cbd4058fc058a0d277d423b9ff0128d9dc23609
fedec1 <federico.colantuono@edu.itspiemonte.it>
2024-06-12 15:55:10 +0200

    inizializzazione

---
+++ src/main/java/com/fedec/Prj21QuizCountriesApplication.java
@@ -0,0 +1,13 @@
+package com.fedec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj21QuizCountriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj21QuizCountriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fedec/Prj21QuizCountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj21QuizCountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
