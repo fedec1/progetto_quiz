5a3d35432fb4541d7677342856c18738173348e9
fedec1 <federico.colantuono@edu.itspiemonte.it>
2024-06-12 17:26:25 +0200

    bandierine aggiunte

--- src/main/java/com/fedec/entities/Nazione.java
+++ src/main/java/com/fedec/entities/Nazione.java
@@ -1,10 +1,13 @@
 package com.fedec.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "countriesv2")
 public class Nazione {
 	
+	@Id
+	private int id;
 }
