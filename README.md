b4b0ee6f6bd829695bf5f2a21338763e13431ac1
fedec1 <federico.colantuono@edu.itspiemonte.it>
2024-06-13 18:50:40 +0200

    Documentazione WIP e commenti

---
+++ README.md
@@ -0,0 +1,19 @@
+# Progetto Quiz Nazioni
+Sviluppo di un'applicazione web che mostri 15 domande a risposta multipla all'utente riguardanti capitali e stati confinanti e mostri un punteggio finale
+
+## Collaboratori
+
+- Federico Colantuono
+- William Fernandez
+- Gianluca Fiore
+- Daniele Panzica
+
+## Fasi di sviluppo
+
+### Architettura del Database
+
+Il database è stato fornito inizialmente con i seguenti campi:
+
+- *alpha2code* : contiene gli identificativi a due lettere per le nazioni (es. IT)
+- *alpha3code* : contiene gli identificativi a tre lettere per le nazioni (es. ITA)
+- *altSpellings* : contiene una lista di nomi alternativi per la nazione
--- src/main/java/accessori/FlagImporter.java
+++ src/main/java/accessori/FlagImporter.java
@@ -17,42 +17,42 @@ import com.fedec.services.NazioneService;
 public class FlagImporter {
 
     public static void main(String[] args) {
-        SpringApplication.run(FlagImporter.class, args);
+        SpringApplication.run(FlagImporter.class, args); //esecuzione dell'applicazione
     }
 
     @Autowired
-    private NazioneService nazioneService;
+    private NazioneService dao;
 
     @Bean
     public CommandLineRunner importBandiere() {
         return args -> {
-            String folderPath = ".\\maboglia country-flags main svg";
-            File folder = new File(folderPath);
-            File[] listOfFiles = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".svg"));
+            String folderPath = ".\\maboglia country-flags main svg"; //percorso relativo della cartella contenente le bandiere
+            File folder = new File(folderPath); //crea un nuovo oggetto file, con percorso la cartella delle bandiere
+            File[] listOfFiles = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".svg")); //se il file termina per .svg, esso viene aggiunto all'array di files
 
             if (listOfFiles != null) {
-                Map<String, String> alphaCodeToFilePathMap = new HashMap<>();
-                for (File file : listOfFiles) {
+                Map<String, String> alphaCodeToFilePathMap = new HashMap<>(); //crea una nuova hashmap che mappa gli alphacode2 ai nomi dei file nell'array
+                for (File file : listOfFiles) { //per ogni file dell'array
                     if (file.isFile()) {
-                        String fileName = file.getName();
-                        String alphaCode = fileName.substring(0, 2).toUpperCase();
-                        alphaCodeToFilePathMap.put(alphaCode, file.getAbsolutePath());
+                        String fileName = file.getName(); //restituisce il nome del file(ad esempio it.svg)
+                        String alphaCode = fileName.substring(0, 2).toUpperCase(); //prende i primi due caratteri del file e li converte in maiusc
+                        alphaCodeToFilePathMap.put(alphaCode, file.getAbsolutePath()); //mappa l'alphacode al percorso assoluto del file
                     }
                 }
 
-                for (Map.Entry<String, String> entry : alphaCodeToFilePathMap.entrySet()) {
-                    String alphaCode = entry.getKey();
-                    String filePath = entry.getValue();
+                for (Map.Entry<String, String> entry : alphaCodeToFilePathMap.entrySet()) { // per ogni entry della mappa precedente (coppia chiave valore, ad es 'IT' : '/percorso'
+                    String alphaCode = entry.getKey(); //estrae la key (alphacode2)
+                    String filePath = entry.getValue(); //estrae il value (percorso file)
 
-                    Nazione nazione = nazioneService.getNazioneByAlpha2Code(alphaCode);
-                    if (nazione != null) {
-                        nazione.setBandiera(filePath);
-                        nazioneService.saveNazione(nazione);
+                    Nazione nazione = dao.getNazioneByAlpha2Code(alphaCode); //istanza oggetto nazione cercata per alphacode corrispondende
+                    if (nazione != null) { //se c'è
+                        nazione.setBandiera(filePath); //setta la bandiera con il percorso file
+                        dao.saveNazione(nazione); //aggiorna il record corrispondente
                     }
                 }
                 System.out.println("Aggiornamento del database completato con successo.");
             } else {
-                System.out.println("La cartella specificata non contiene file SVG.");
+                System.out.println("La cartella specificata non contiene file SVG."); //listoffiles è null
             }
         };
     }
