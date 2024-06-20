5f77ac74337c68a801f45d8cece017b781e45ed8
fedec1 <federico.colantuono@edu.itspiemonte.it>
2024-06-20 15:11:02 +0200

    inizio implementazione

--- src/main/java/com/fedec/repos/SessioneDAO.java
+++ src/main/java/com/fedec/repos/SessioneDAO.java
@@ -1,9 +1,14 @@
 package com.fedec.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fedec.entities.Sessione;
 
 public interface SessioneDAO extends JpaRepository<Sessione, Integer> {
+	
+	List<Sessione> findByDifficulty(int difficulty);
+	List<Sessione> findByType(String type);
 
 }
--- src/main/java/com/fedec/services/SessioneService.java
+++ src/main/java/com/fedec/services/SessioneService.java
@@ -1,7 +1,13 @@
 package com.fedec.services;
 
+import java.util.List;
+
+import com.fedec.entities.Sessione;
+
 public interface SessioneService {
 
 	public void saveSession(String username, String quizType, int punteggio, int difficulty);
+	List<Sessione> findByDifficulty(int difficulty);
+	List<Sessione> findByType(String type);
 	
 }
